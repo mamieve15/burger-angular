d66f09c94f4fe52602833c6f16ebac8a6483b1d0
mamieve15 <awaba1810@gmail.com>
2020-05-11 05:07:12 +0200

    css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
 import { AppComponent } from './app.component';
-import { ApiModule } from './swagger';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { ListBurgersComponent } from './burgers/burgers.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatExpansionModule} from '@angular/material/expansion';
-
-import { BurgerDetailsComponent } from './burger-details/burger-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ListBurgersComponent,
-    BurgerDetailsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    ApiModule,
-    HttpClientModule,
-    AppRoutingModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatButtonModule,
-    MatExpansionModule
+    AppRoutingModule // CLI adds AppRoutingModule to the AppModule's imports array
   ],
   providers: [],
   bootstrap: [AppComponent]
